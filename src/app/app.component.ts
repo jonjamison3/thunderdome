9fc8603659f7c7fbecb161eb1eecc37aba92293f
jonjamison3 <jonjamison3@gmail.com>
2016-09-22 22:03:49 -0400

    menu component addition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/primeng';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private items: MenuItem[]; 
+  
+  private activeItem: MenuItem;
   title = 'Thunderdome!';
+  ngOnInit(){
+    this.items = [
+      {label: 'About', icon: 'fa-book', routerLink: ['about']},
+      {label: 'Apiz', icon: 'fa-twitter', routerLink: ['apiz']}
+    ];
+
+    this.activeItem = this.items[0];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { ApizComponent } from './apiz/apiz.component';
 import { AboutComponent } from './about/about.component';
-
+import {TabMenuModule, MenuItem} from 'primeng/primeng';
 @NgModule({
   declarations: [
     AppComponent,
     ApizComponent,
-    AboutComponent
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    routing,
+    TabMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
