eb1d6c43d5c8b70a806545e0183f2828dc9d410d
jonjamison3 <jonjamison3@gmail.com>
2016-09-25 17:00:22 -0400

    pulling some tracks down

--- src/app/apiz/apiz.component.ts
+++ src/app/apiz/apiz.component.ts
@@ -1,19 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import {SpotifyService} from  './spotify.service';
+import { Observable }        from 'rxjs/Observable';
+import { Subject }           from 'rxjs/Subject';
 @Component({
   selector: 'app-apiz',
   templateUrl: './apiz.component.html',
   styleUrls: ['./apiz.component.css']
 })
 export class ApizComponent implements OnInit {
-  search: String; 
+  // /private searchTerm = new Subject<string>();
+  songs: any;
   constructor(public spotifyService: SpotifyService) { }
 
-  ngOnInit() {
-    this.search = 'Some search term';
+  ngOnInit(): void {
+    this.songs = this.spotifyService.getSongs(); 
+    // this.songs = this.searchTerm  // wait for 300ms pause in events
+    //   .distinctUntilChanged()   // ignore if next search term is same as previous
+    //   .switchMap(searchTerm => searchTerm   // switch to new observable each time
+    //   // return the http search observable
+    //     ? this.spotifyService.songSearch(searchTerm)
+    //     // or the observable of empty songs if no search term
+    //     : Observable.of<any[]>([]));
   }
-  spotifySongSearch(search:String){
-    this.spotifyService.songSearch(search);
-  }
-
+  // songSearch(searchTerm: Subject<string>): void {
+  //   //this.searchTerm.next(term);
+  //   this.songs = searchTerm
+  //     .debounceTime(300)        // wait for 300ms pause in events
+  //     .distinctUntilChanged()   // ignore if next search term is same as previous
+  //     .switchMap(searchTerm => searchTerm   // switch to new observable each time
+  //     // return the http search observable
+  //       ? this.spotifyService.songSearch(searchTerm)
+  //       // or the observable of empty songs if no search term
+  //       : Observable.of<any[]>([]));
+  // }
+  
 }
--- src/app/apiz/spotify.service.ts
+++ src/app/apiz/spotify.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable }        from 'rxjs/Observable';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class SpotifyService {
   terms: String; 
-  constructor() { }
-  songSearch (term: String){
-    this.terms = term; 
-    console.log(term);
+  response: any; 
+  constructor(public http: Http) { }
+  // songSearch (term: String):Observable<any[]>{
+  //   this.terms = term;
+  //   this.response = this.http
+  //              .get(``)
+  //              .map((r: Response) => r.json().data);
+  //   console.log(this.response);
+  //   return this.response;
+  // }
+  getSongs(): Promise.resolve<any>{
+    return this.http.get('http://api.spotify.com/v1/search?q=some&type=track').toPromise().then(res=>res.json().tracks as any);
   }
+
 }
