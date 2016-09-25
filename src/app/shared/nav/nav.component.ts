2e5d07ba5c5677f814a9a863badcf0dfe18e7e8f
jonjamison3 <jonjamison3@gmail.com>
2016-09-25 01:54:19 -0400

    l2bootstrap

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  @Input()
+  title: String;
   private items: any[];
   private activeItem: any;
   ngOnInit(){
