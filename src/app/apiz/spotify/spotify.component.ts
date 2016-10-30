8a666cf1a4257b68ca02e8aebf3a7740cf13d99d
jonjamison3 <jonjamison3@gmail.com>
2016-10-29 21:16:54 -0400

    windows commit

--- src/app/apiz/spotify/spotify.component.ts
+++ src/app/apiz/spotify/spotify.component.ts
@@ -7,19 +7,20 @@ import { Observable }        from 'rxjs/Observable';
   styleUrls: ['./spotify.component.css']
 })
 export class SpotifyComponent implements OnInit {
- // /private searchTerm = new Subject<string>();
+  songs: any[]; 
+  private searchTerm = new Subject<string>();
   tracks: Observable<any[]>;
   constructor(public spotifyService: SpotifyService) { }
 
   ngOnInit(): void {
-    //this.spotifyService.getSetTracks().subscribe(tracks=> this.tracks = tracks);
-    // this.songs = this.searchTerm  // wait for 300ms pause in events
-    //   .distinctUntilChanged()   // ignore if next search term is same as previous
-    //   .switchMap(searchTerm => searchTerm   // switch to new observable each time
-    //   // return the http search observable
-    //     ? this.spotifyService.songSearch(searchTerm)
-    //     // or the observable of empty songs if no search term
-    //     : Observable.of<any[]>([]));
+    this.spotifyService.getSetTracks().subscribe(tracks=> this.tracks = tracks);
+    this.songs = this.searchTerm  // wait for 300ms pause in events
+      .distinctUntilChanged()   // ignore if next search term is same as previous
+      .switchMap(searchTerm => searchTerm   // switch to new observable each time
+      // return the http search observable
+        ? this.spotifyService.songSearch(searchTerm)
+        // or the observable of empty songs if no search term
+        : Observable.of<any[]>([]));
   }
   // songSearch(searchTerm: Subject<string>): void {
   //   //this.searchTerm.next(term);
